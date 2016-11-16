598ac45d859cf3c4533d093c66d74f016ec8d69e
kasoya <shuyang_x@hotmail.com>
2016-11-16 21:01:12 +0800

    corejava-ch01-day01-1-第1.8节数组

--- workshop/core/ch01/CumulateDaily.java
+++ workshop/core/ch01/CumulateDaily.java
@@ -1,40 +1,75 @@
-import java.util.Arrays;
+import java.util.*;
+
 public class CumulateDaily{
   public static void main(String[] args) {
-/*    
-    float f=123456789;
-    System.out.println(f);//1.23456792E8
 
-    String greeting="Hello, world!";
-    String location=greeting.substring(7,12);
-    System.out.println(location);
-
-    String names="aaa, bbb, ccc";
-    String[] res=names.split(", ");
-    System.out.println(Arrays.toString(res));
-
-    if(location.equals("world")) //if location=null : NullPointerException
-      System.out.println("true");
-
-    if("world".equals(location)) //works well even if location=null
-      System.out.println("true");
-
-    String first="come";
-    String second="Amy";
-    System.out.println(first.compareTo(second));
-
-    int n=42;
-    String str=Integer.toString(n);
-    System.out.println(str);
-    str=Integer.toString(n,2);
-    System.out.println(str);
-    str=n+"";
-    System.out.println(str);
-
-    n=Integer.parseInt(str);
-    System.out.println(n+5);//more: Double.toString(), Double.parseDouble()
-
-    //really necessary??
-*/
   }
 }
+    // using ArrayList-------------------------------
+    // ArrayList<String> friends;
+    // friends = new ArrayList<>();
+    // friends.add("peter");
+    // friends.add("King");
+    // for (int i=0; i<friends.size(); i++) {
+    //     System.out.println(friends.get(i));
+    // }
+    // System.out.println("--------------------------");
+    // friends.remove(1);
+    // for (int i=0; i<friends.size(); i++) {
+    //     System.out.println(friends.get(i));
+    // }
+    // System.out.println("--------------------------");
+    // friends.add(0,"geek");
+    // for (int i=0; i<friends.size(); i++) {
+    //     System.out.println(friends.get(i));
+    // }
+    // System.out.println("--------------------------");
+    // String first=friends.get(0);
+    // friends.set(1,"Mary");
+    // System.out.printf("now first is %s\n",first);
+    // System.out.printf("now second is %s\n",friends.get(1));
+
+    // Random gen = new Random();
+    // int i=10;
+    // while(i>0){
+    //     System.out.println(gen.nextInt(10));
+    //     i--;
+    // }
+    // Scanner in=new Scanner(System.in);
+    // System.out.println("input your age:");
+    // int age=in.nextInt();
+    // System.out.printf("you are %d years old.\n",age);
+
+    // float f=123456789;
+    // System.out.println(f);//1.23456792E8
+
+    // String greeting="Hello, world!";
+    // String location=greeting.substring(7,12);
+    // System.out.println(location);
+
+    // String names="aaa, bbb, ccc";
+    // String[] res=names.split(", ");
+    // System.out.println(Arrays.toString(res));
+
+    // if(location.equals("world")) //if location=null : NullPointerException
+    //   System.out.println("true");
+
+    // if("world".equals(location)) //works well even if location=null
+    //   System.out.println("true");
+
+    // String first="come";
+    // String second="Amy";
+    // System.out.println(first.compareTo(second));
+
+    // int n=42;
+    // String str=Integer.toString(n);
+    // System.out.println(str);
+    // str=Integer.toString(n,2);
+    // System.out.println(str);
+    // str=n+"";
+    // System.out.println(str);
+
+    // n=Integer.parseInt(str);
+    // System.out.println(n+5);//more: Double.toString(), Double.parseDouble()
+
+    // //really necessary??
