6f869d3265dd51ce666007509d54c91c032d8032
kasoya <shuyang_x@hotmail.com>
2016-11-16 16:29:07 +0800

    corejava-ch01-day01

---
+++ workshop/core/ch01/CumulateDaily.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+public class CumulateDaily{
+  public static void main(String[] args) {
+/*    
+    float f=123456789;
+    System.out.println(f);//1.23456792E8
+
+    String greeting="Hello, world!";
+    String location=greeting.substring(7,12);
+    System.out.println(location);
+
+    String names="aaa, bbb, ccc";
+    String[] res=names.split(", ");
+    System.out.println(Arrays.toString(res));
+
+    if(location.equals("world")) //if location=null : NullPointerException
+      System.out.println("true");
+
+    if("world".equals(location)) //works well even if location=null
+      System.out.println("true");
+
+    String first="come";
+    String second="Amy";
+    System.out.println(first.compareTo(second));
+
+    int n=42;
+    String str=Integer.toString(n);
+    System.out.println(str);
+    str=Integer.toString(n,2);
+    System.out.println(str);
+    str=n+"";
+    System.out.println(str);
+
+    n=Integer.parseInt(str);
+    System.out.println(n+5);//more: Double.toString(), Double.parseDouble()
+
+    //really necessary??
+*/
+  }
+}
