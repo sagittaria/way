cd1e1a5fac5cc2c0486496c8490976c3242e7a6d
kasoya <shuyang_x@hotmail.com>
2016-11-30 21:28:49 +0800

    corejava-ch03-day06-2-随手多做11个习题

--- workshop/core/ch01/Employee.java
+++ workshop/core/ch01/Employee.java
@@ -1,12 +1,15 @@
 
 public class Employee implements Measurable{
 	private String name;
-	private double salary;
+	private double salary=0.0;
 	public Employee(String name, double salary){
 		this.name=name;
 		this.salary=salary;
 	}
 	public double getMeasure(){
 		return salary;
+	}
+	public String getName(){
+		return name;
 	}
 }
--- workshop/core/ch01/ExerciseCh03.java
+++ workshop/core/ch01/ExerciseCh03.java
@@ -1,10 +1,13 @@
 public class ExerciseCh03{
     public static void main(String[] args) {
-    	//1
-    	Employee[] emps={new Employee("aaa",1000),new Employee("bbb",2000),new Employee("aaa",6000)};
+    	//1,2
+    	Employee[] emps={new Employee("aaa",1000),new Employee("bbb",2000),new Employee("ccc",6000)};
     	double avg=average(emps);
     	System.out.println(avg);
-    	System.out.println("--------EX01");
+    	System.out.println("--------EX01↑");
+    	Employee e=(Employee)largest(emps);
+    	System.out.println(e.getName());
+    	System.out.println("--------EX02↑");
     }
     public static double average(Measurable[] objects){
 		double sum=0;
@@ -13,4 +16,15 @@ public class ExerciseCh03{
 		}
 		return objects.length==0 ? 0 : sum/objects.length;
 	}
+    public static Measurable largest(Measurable[] objects){
+    	double maxSalary=0;
+    	Measurable tmpE=null;
+		for(Measurable e : objects){
+			if(e.getMeasure()>maxSalary){
+				maxSalary=e.getMeasure();
+				tmpE=e;
+			}
+		}
+		return tmpE;
+    }
 }
