10c527b815069526eb881073c8728d239162e265
kasoya <shuyang_x@hotmail.com>
2016-12-05 14:20:44 +0800

    mooc-java-集合set、散列hashmap

---
+++ workshop/core/ch01/Mooc.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+
+//慕课-翁恺java
+public class Mooc {
+	private HashMap<Integer, String> month = new HashMap<>();
+	
+	public Mooc(){
+		month.put(1,"Jan");
+		month.put(2,"Feb");
+		month.put(3,"Mar");
+		month.put(4,"si yue");
+		month.put(4,"Apr");//因为键唯一，所以“Apr”会覆盖“si yue”
+		System.out.println(month.keySet().size());//输出里面有几项
+		for(Integer k : month.keySet()){
+			System.out.println(month.get(k));
+		}//HashMap的遍历
+	}
+	
+	public String getMonthName(int n){
+		if(month.containsKey(n))
+			return month.get(n);
+		else
+			return "Not Found";
+	}
+	
+	public static void main(String[] args){
+		Mooc m = new Mooc();
+		System.out.println("---------------");
+		System.out.println(m.getMonthName(4));
+		System.out.println(m.getMonthName(3));
+		System.out.println(m.getMonthName(5));
+		System.out.println(m.getMonthName(2));
+		System.out.println(m.getMonthName(1));
+	}
+}
