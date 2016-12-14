757c3ff7c17a8908ea8ce5173ca54d10a6d68631
kasoya <shuyang_x@hotmail.com>
2016-12-14 20:53:31 +0800

    mooc-java-狐狸与兔子-以后还会再看的

--- workshop/core/ch01/field/Field.java
+++ workshop/core/ch01/field/Field.java
@@ -5,6 +5,11 @@ import java.util.ArrayList;
 import cell.Cell;
 
 public class Field {
+	private static final Location[] adjacent = {
+		new Location(-1,-1),new Location(-1,0),new Location(-1,1),
+		new Location(0,-1), new Location(0,0), new Location(0,1),
+		new Location(1,-1), new Location(1,0), new Location(1,1)
+	};
 	private int width;
 	private int height;
 	private Cell[][] field;
@@ -42,11 +47,56 @@ public class Field {
 		return list.toArray(new Cell[list.size()]);
 	}
 	
+	public Location[] getFreeNeighbour(int row, int col) {
+		ArrayList<Location> list = new ArrayList<Location>();
+		for ( Location loc : adjacent ) {
+			int r = row+loc.getRow();
+			int c = col+loc.getCol();
+			if ( r >-1 && r<height && c>-1 && c<width && field[r][c] == null ) {
+				list.add(new Location(r,c));
+			}
+		}
+		return list.toArray(new Location[list.size()]);
+	}
+	
+	public boolean placeRandomAdj(int row, int col, Cell cell) {
+		boolean ret = false;
+		Location[] freeAdj = getFreeNeighbour(row, col);
+		if ( freeAdj.length >0 ) {
+			int idx = (int)(Math.random()*freeAdj.length);
+			field[freeAdj[idx].getRow()][freeAdj[idx].getCol()] = cell;
+			ret = true;
+		}
+		return ret;
+	}
+	
+	public Cell remove(int row, int col) {
+		Cell ret = field[row][col];
+		field[row][col] = null;
+		return ret;
+	}
+	
+	public void remove(Cell cell) {
+		for ( int row = 0; row < height; row++ ) {
+			for ( int col = 0; col < width; col++ ) {
+				if ( field[row][col] == cell ) {
+					field[row][col] = null;
+					break;
+				}
+			}
+		}
+	}
+	
 	public void clear() {
 		for ( int i=0; i<height; i++ ) {
 			for ( int j=0; j<width; j++ ) {
 				field[i][j] = null;
 			}
 		}
+	}
+
+	public void move(int row, int col, Location loc) {
+		field[loc.getRow()][loc.getCol()] = field[row][col];
+		remove(row, col);
 	}
 }
