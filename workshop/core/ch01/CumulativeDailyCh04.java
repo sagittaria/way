129fe80e704d71edcc417353f2b46a21a7c61d90
kasoya <shuyang_x@hotmail.com>
2016-12-16 16:48:02 +0800

    corejava-枚举类型(书&官档)-day13-1-第4章过遍

--- workshop/core/ch01/CumulativeDailyCh04.java
+++ workshop/core/ch01/CumulativeDailyCh04.java
@@ -1,4 +1,16 @@
 public class CumulativeDailyCh04 {
+	private enum Size {BIG, MEDIUM, LARGE};
+	
+	public enum Day {
+	    SUNDAY, MONDAY, TUESDAY, WEDNESDAY,
+	    THURSDAY, FRIDAY, SATURDAY 
+	}
+	Day day;
+		
+	public CumulativeDailyCh04(Day day){
+		this.day = day;
+	}
+	
 	public static void main(String[] args) {
 		Emp e = new Boss();
 		e.setTestStr("protected - test str");
@@ -9,6 +21,41 @@ public class CumulativeDailyCh04 {
 		}
 		System.out.println(e.testStr);
 		System.out.println(e.getName());
+		
+		for(Size s : Size.values()){
+			System.out.println(s);
+		}
+		
+		Object obj = new Emp();
+		Class<?> cl = obj.getClass();
+		System.out.println("Class name: "+cl.getName());
+		
+		//从官方文档补Enum type(http://docs.oracle.com/javase/tutorial/java/javaOO/enum.html)
+		CumulativeDailyCh04 today = new CumulativeDailyCh04(Day.TUESDAY);
+		today.tell();
+		CumulativeDailyCh04 yesterday = new CumulativeDailyCh04(Day.MONDAY);
+		yesterday.tell();
+		CumulativeDailyCh04 zhouliu = new CumulativeDailyCh04(Day.SATURDAY);
+		zhouliu.tell();
+		CumulativeDailyCh04 workday = new CumulativeDailyCh04(Day.THURSDAY);
+		workday.tell();
+	}
+	
+	public void tell() {
+		switch (day) {
+		case MONDAY:
+			System.out.println("星期一");
+			break;
+		case TUESDAY:
+			System.out.println("周二");
+			break;
+		case SATURDAY:
+		case SUNDAY:
+			System.out.println("周末！");
+			break;
+		default:
+			System.out.println("平常工作日，就这样吧");
+		}
 	}
 }
 
