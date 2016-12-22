8a56b665c3594714d12dd5f896e6eed48271721a
kasoya <shuyang_x@hotmail.com>
2016-12-22 14:44:12 +0800

    c_grammar-ad-链表-把add操作单独做个函数出来

--- C_grammar/node.c
+++ C_grammar/node.c
@@ -8,6 +8,8 @@
 //     struct _node *next;
 // } Node;
 
+Node* add(Node* head, int number);
+
 int main()
 {
     Node *head = NULL;
@@ -15,23 +17,28 @@ int main()
     do{
         scanf("%d", &number);
         if(number != -1){
-            // add to linked-list
-            Node *p = (Node *)malloc(sizeof(Node));
-            p->value = number;
-            p->next = NULL;
-            // find the last
-            Node *last = head;
-            if(last != NULL){//if p is NOT the first node
-                while(last->next != NULL){
-                    last = last->next;
-                }
-                // attach
-                last->next = p;
-            }else{//else if p is the first node
-                head = p;
-            }
+            head = add(head, number);
         }
     } while (number != -1);
 
     return 0;
+}
+
+Node* add(Node* head, int number){
+    // add to linked-list
+    Node *p = (Node *)malloc(sizeof(Node));
+    p->value = number;
+    p->next = NULL;
+    // find the last
+    Node *last = head;
+    if(last != NULL){//if p is NOT the first node
+        while(last->next != NULL){
+            last = last->next;
+        }
+    // attach
+        last->next = p;
+    }else{//else if p is the first node
+        head = p;
+    }
+    return head;
 }
